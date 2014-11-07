bbff4506ecc8cd2755782e8aa58eb0e275f65fa6
Jonathan Chavez <jchavezlopez9@gmail.com>
2014-11-07 14:23:39 -0600

    Cambios en Perfil

--- perfil.php
+++ perfil.php
@@ -130,6 +130,178 @@
                     <div class="col-md-2">
                     </div>
                   </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Tel&eacute;fono</label>
+                    </div>
+                    <div class="col-md-2">
+                      <input type="text" name="caract" placeholder="Caract">
+                    </div>
+                    <div class="col-md-3">
+                      <input type="text" name="número" placeholder="Número">
+                    </div>
+                    <div class="col-md-2">
+                    </div>
+                  </div>
+                </section>
+                <section>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h2>Datos adicionales como profesional</h2>
+                      <hr>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Profesional</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="profesional" placeholder="Sleccione el tipo de profesional">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                    </div>
+                    <div class="col-md-7">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox" name="prof"> Quiero ofrecer mis actividades profesionales en Industria en la Web
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox" name="prof"> Estoy buscando empleo y quiero recibir ofertas
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Educaci&oacute;n</label>
+                    </div>
+                    <div class="col-md-7">
+                      <textarea class="form-control" rows="3"></textarea>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Referencias</label>
+                    </div>
+                    <div class="col-md-7">
+                      <textarea class="form-control" rows="3"></textarea>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Experiencia Laboral</label>
+                    </div>
+                    <div class="col-md-7">
+                      <textarea class="form-control" rows="3"></textarea>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Objetivo Laboral</label>
+                    </div>
+                    <div class="col-md-7">
+                      <textarea class="form-control" rows="3"></textarea>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Comentarios Adicionales</label>
+                    </div>
+                    <div class="col-md-7">
+                      <textarea class="form-control" rows="3"></textarea>
+                    </div>
+                  </div>
+                </section>
+                <section>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <h2>Datos de la Empresa</h2>
+                      <hr>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Nombre Fantas&iacute;a de la Empresa</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="name_business">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Ubicaci&oacute;n Real de la Empresa</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="address_business">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Ubicaci&oacute;n de la Empresa</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="address_business2">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Facebook</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="facebook">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Twitter</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="twitter">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Web</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="web">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Tel&eacute;fono</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="telephone">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Email</label>
+                    </div>
+                    <div class="col-md-7">
+                      <input type="text" name="email">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-5">
+                      <label>Acerca de la Empresa</label>
+                    </div>
+                    <div class="col-md-7">
+                      <textarea class="form-control" rows="3"></textarea>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="imagefile">
+                        <input type="file" name="fileImage" id="files">
+                      </div>
+                    </div>
+                  </div>
                 </section>
               </form>
 
